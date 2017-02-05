302cbcdf8ea4cd139e41176713a37ce82a35a132
sakib3 <sakib3@gmail.com>
2017-02-05 03:56:06 +0100

    proj init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  restaurants: any;
+  isRecentOrder: Boolean = false;
+  constructor(public navCtrl: NavController) {
+    this.restaurants = [
+      { id: 1, name: 'Macdonalds' },
+      { id: 2, name: 'Max' },
+      { id: 3, name: 'Burger King' },
+      { id: 4, name: 'Subway' }];
+    setTimeout(() =>{
+      this.isRecentOrder = true;
+    },2000);
+  }
+  showOrder(): void { }
+  selectShopForOrder(shopId): void { console.log(shopId); }
+}
